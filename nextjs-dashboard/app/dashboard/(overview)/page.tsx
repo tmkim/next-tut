5939b3b2063aa4671b0b2e4d08e74acb320ccba7
tmkim <taeminkim13@gmail.com>
2024-05-28 03:26:09 -0400

    added metadata

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -7,6 +7,11 @@ import { RevenueChartSkeleton,
          LatestInvoicesSkeleton,
          CardsSkeleton } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
 
 export default async function Page() {
     // const revenue = await fetchRevenue();
--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,5 +1,15 @@
+import { Metadata } from 'next';
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'yessir',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh')
+};
 
 export default function RootLayout({
   children,
